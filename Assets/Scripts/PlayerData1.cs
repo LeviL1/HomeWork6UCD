5b9759ef726b78bb3ddc73fda3d956a3f2609428
LeviL1 <78377282+LeviL1@users.noreply.github.com>
2021-04-16 10:53:49 -0500

    Update PlayerData1.cs
    removing monobehaviour because it messed everything up

--- Assets/Scripts/PlayerData1.cs
+++ Assets/Scripts/PlayerData1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class PlayerData1 : MonoBehaviour
+public class PlayerData1
 {
   public int level;
   public int health;
